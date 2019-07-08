35ee421b86a71bef02a3aaa5e83f8f1cfc9459be
Jeffreymurray95 <Jeffreymurray95@gmail.com>
2019-07-08 17:29:30 -0400

    add proj
    ffdg

---
+++ Garret/Assets/Dollardrop.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dollardrop : MonoBehaviour
+{
+    public Transform spawnpoint;
+    public Rigidbody dollar;
+
+ 
+    void Update()
+    {
+        
+        transform.position+=new Vector3(0.0f, -0.2f, 0.0f);
+        Rigidbody rb;
+        int count=0;
+
+        if (Input.GetKey(KeyCode.A) )
+        rb = Instantiate(dollar, transform.position, spawnpoint.rotation) as Rigidbody;
+        count++;
+
+    }
+}
